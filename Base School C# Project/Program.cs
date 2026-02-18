411f78e68a904fdd7df545fc6737247f1ae24390
Lachlan Gardner <lachlan.gardner@student.onslow.school.nz>
2026-02-18 13:01:00 +1300

    Started Classes

--- Base School C# Project/Program.cs
+++ Base School C# Project/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Common;
+
+public struct Student
+{
+    public int id;
+
+    public string name;
+
+    public string NSN;
+
+    public string email;
+
+    public int age;
+
+    public Student(int idNumber, string studentName, string studentNSN, string studentEmail, int studentAge)
+    {
+        id = idNumber;
+        name = studentName;
+        NSN = studentNSN;
+        email = studentEmail;
+        age = studentAge;
+    }
+
+    static void Main(string[] args)
+    {
+        Student lachlan = new(123, "Lachlan", "NSN1234", "lachlan.gardner@student.onslow.school.nz", 17);
+
+        Console.WriteLine(lachlan.id);
+        Console.WriteLine(lachlan.name);
+        Console.WriteLine(lachlan.NSN);
+        Console.WriteLine(lachlan.email);
+        Console.WriteLine(lachlan.age);
+        
+    }
+}
