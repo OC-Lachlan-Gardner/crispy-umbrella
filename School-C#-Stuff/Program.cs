9e0be06ccb90359c5ae6f4116273f4877fc71f1c
Lachlan Gardner <lachlan.gardner@student.onslow.school.nz>
2026-02-23 15:02:55 +1300

    Finished Task A

---
+++ School-C#-Stuff/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.InteropServices;
+
+class Book
+{
+    public string Title;
+
+    public string Author;
+
+    public int Pages;
+
+    public Book(string bookTitle, string bookAuthor, int bookPages)
+    {
+        Title = bookTitle;
+
+        Author = bookAuthor;
+
+        Pages = bookPages;
+    }
+
+    public string summary()
+    {
+        return $"{Title} by {Author} is {Pages} pages long";
+    }
+}
+
+class Program
+{
+    public static string BookSummary(Book book)
+    {
+        return $"{book.Title} by {book.Author} is {book.Pages} pages long";   
+    }
+    static void Main(string[] args)
+    {
+        Book vicious = new Book("Vicious", "V. E. Schwab", 402);
+
+        Book vengeful = new Book("Vengeful", "V. E. Schwab", 462);
+
+        Console.WriteLine(BookSummary(vicious));
+
+        Console.WriteLine(vengeful.summary());
+
+        // The instance method looks a lot cleaner because it contains the function within itself, since it is useless outside of that context..
+    }
+}
