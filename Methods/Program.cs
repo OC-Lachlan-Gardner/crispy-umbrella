267776ad71310e4973622f0dcb19e6e876b3fc12
Lachlan Gardner <lachlan.gardner@student.onslow.school.nz>
2026-02-25 13:01:46 +1300

    Instance creatioin needs work

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,7 +1,49 @@
-﻿class Program
+﻿class Timer
+{
+    public bool IsRunning = false;
+
+    public int Seconds = 0;
+
+    public void Start()
+    {
+        IsRunning = true;
+    }
+
+    public void Tick()
+    {
+        if (IsRunning)
+        {
+            Seconds++;
+        }
+    }
+
+    public void Reset()
+    {
+        IsRunning = false;
+
+        Seconds = 0;
+    }
+
+    public static void Update()
+    {
+        Console.WriteLine($"Timer runner: {IsRunning}");
+        Console.WriteLine($"{Seconds} seconds have passed");
+    }
+}
+
+class Program
 {
     public static void Main(string[] args)
     {
-        
+        Timer newTimer = new Timer();
+
+        newTimer.Start();
+        newTimer.Update();
+
+        newTimer.Tick();
+        newTimer.Update();
+
+        newTimer.Reset();
+        newTimer.Update();
     }
 }
