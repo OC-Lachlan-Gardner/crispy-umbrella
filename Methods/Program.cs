a91f201bffbc5dd65c3ff24d1677b4d44c33bce2
Lachlan Gardner <lachlan.gardner@student.onslow.school.nz>
2026-02-25 20:57:27 +1300

    Finished Task E

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,7 +1,37 @@
-﻿class Program
+﻿class Badge
+{
+    public string name;
+
+    public int level;
+
+    public string Label;
+
+    public Badge(string badgeName, int badgeLevel)
+    {
+        name = badgeName;
+
+        level = badgeLevel;
+
+        Label = $"{name} - Level {level}";
+    }
+
+    public string label()
+    {
+        return $"{name} - Level {level}";
+    }
+}
+class Program
 {
     public static void Main(string[] args)
     {
-        
+        Badge newBadge = new Badge("Ranger", 4);
+
+        Console.WriteLine(newBadge.label());
+
+        Badge newerBadge = new Badge("Mage", 9);
+
+        Console.WriteLine(newerBadge.Label);
+
+
     }
 }
