62d7931fe46f961f07c346d278ee186b70371f84
Lachlan Gardner <lachlan.gardner@student.onslow.school.nz>
2026-03-11 12:21:43 +1300

    Fixed solution

--- School-Programming-Project/obj/Debug/net9.0/School-Programming-Project.AssemblyInfo.cs
+++ School-Programming-Project/obj/Debug/net9.0/School-Programming-Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("School-Programming-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+229050d1ceb8adef4d0fbb6fe7b6cf58bd0243ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13a9a1947830c2fe15483bd74b641a6099ba7e01")]
 [assembly: System.Reflection.AssemblyProductAttribute("School-Programming-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("School-Programming-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
