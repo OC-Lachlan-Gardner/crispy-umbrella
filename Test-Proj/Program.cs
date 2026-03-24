063388bf401346aab68df987e8b6993e7e648c1e
Lachlan Gardner <lachlan.gardner@student.onslow.school.nz>
2026-03-25 12:29:32 +1300

    tested aggregate

---
+++ Test-Proj/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> test = [0, 9, 34, 42, -9];
+
+        Console.WriteLine(test.Aggregate((a, b) => a + b));
+    }
+}       
